78910d98a2b7e270ff897845ca5009dd47c5046e
rashmitha <rashmithaananda@gmail.com>
2023-11-28 22:58:30 +0530

    String Palindrome

---
+++ src/Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String str;
+		String strrev ="";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name : ");
+		str = sc.next();
+		
+		int i=str.length()-1;
+		
+		do{
+			
+			strrev= strrev + str.charAt(i);
+			i--;
+		}
+		while(i>=0);
+		
+		if(strrev.equalsIgnoreCase(str)) {
+			System.out.println("Palindrome!");
+		}
+		else
+		{
+			System.out.println("Not a Palindrome");
+		}
+
+	}
+
+}
